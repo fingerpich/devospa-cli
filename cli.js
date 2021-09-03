2bb8c02b9421ebb6893ac0082a6ebe14b23c0f61
fingerpich <zarei.bs@gmail.com>
2021-09-04 01:57:04 +0430

    updated cli to show progress on uploading

--- cli.js
+++ cli.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-const isDev = true
-
 const rimraf = require("rimraf");
 var FormData = require('form-data');
 var fs = require('fs');
@@ -20,20 +18,20 @@ program
 program
   .command("push")
   .arguments('<token>')
+  .arguments('<buildFolder>')
   .description('push command', {
-    token: 'user token'
+    token: 'user token',
+    buildFolder: 'build folder'
   })
-  .option("-d, --directory", "Build Folder")
-  .action((token, options, command) => {
-
-    let { directory = "build" } = options
+  .action((token, buildFolder, options, command) => {
+    console.log(buildFolder + " folder will be zipped and uploaded to devospa.com")
     const api = axios.create({
       maxContentLength: Infinity,
       maxBodyLength: Infinity,
-      baseURL: isDev ? "http://localhost:3020" : "https://devospa.com",
+      baseURL: "http://devospa.com",
+      // baseURL: "http://localhost:3020",
     })
     return api.post("/devospaApi/checkProjectToken", { token }).then(async() => {
-      console.log("token success", token)
       return inquirer.prompt([
         { 
           name: "branch name", 
@@ -43,10 +41,15 @@ program
       .then(async ({'branch name': branchName}) => {
         console.log("Branch Name : ", branchName)
         progressBar.start(100, 1);
-        const zipPath = branchName + token + '.zip'
-        await zip('build', zipPath);
+        const zipPath = "./" + branchName + token + '.zip'
+        await zip(buildFolder, zipPath);
         progressBar.update(10);
-        
+        let n = 10
+        const intervalObj = setInterval(() => {
+          const nn = (100 + n) / 2
+          n += Math.min(10, nn - n)
+          progressBar.update(n)
+        }, 500)
         new Promise((resolve) => {
           const fd = new FormData();
           fd.append("token", token);
@@ -54,21 +57,34 @@ program
           fd.append("file", fs.createReadStream(zipPath));
           fd.pipe(concat({ encoding: 'buffer' }, data => resolve({ data, headers: fd.getHeaders() })));
         }).then(({ data, headers }) => {
-          api.post("/api/upload", data, {
+          return api.post("/devospaApi/upload", data, {
             headers,
-            onUploadProgress: progressEvent => console.log(progressEvent.loaded)
+            onUploadProgress: progressEvent => {
+              var percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total)
+              console.log(percentCompleted)
+              progressBar.update(percentCompleted);
+            }
           }).then((uploadResponse) => {
+            clearInterval(intervalObj)
             progressBar.update(100)
             progressBar.stop(100)
-            console.log("Upload completed, Please check the following address", "https://devospa.com")
+            console.log("Upload completed, Please check devospa.com")
           }).catch(e => {
-            console.error("Upload Error", e)
+            progressBar.stop(0)
+            console.error("Sorry but uploading the zip file errored, Please try again in a few second")
+            process.exit()
           })
         }).catch(err=> {
+          progressBar.stop(0)
           console.error("Errored buffering", err)
+          process.exit()
         }).finally(() => {
+          progressBar.stop(100)
+          
           rimraf(zipPath, (e) => {
             e && console.log(e)
+            console.log("removed created zip file")
+            process.exit()
           })
         })
       })
@@ -80,8 +96,10 @@ program
           // Something else went wrong
         }
       });
+    }).catch(e => {
+      console.error("the project token is wrong, please copy the command from devospa.com")
     })
   })
   
-console.log(process.argv)
+// console.log(process.argv)
 program.parseAsync(process.argv);
