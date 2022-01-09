b1ecae147fa2d88ad9435bcfd711e4247281769d
fingerpich <zarei.bs@gmail.com>
2022-01-09 23:15:17 +0330

    fixed baseurl

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-// export const baseURL = "http://devospa.com"
-export const baseURL = "http://localhost:3020"
+export const baseURL = "http://devospa.com"
+// export const baseURL = "http://localhost:3020"
